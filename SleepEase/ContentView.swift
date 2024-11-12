7c52d3b87362dfe72cea5340540816a2ba65290a
Saloni0512 <sinceresaloni@gmail.com>
2024-11-13 01:18:52 +0530

    Adding base version of the app

--- SleepEase/ContentView.swift
+++ SleepEase/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    @State private var wakeUp = Date.now
+    
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack{
+                Text("When do you wish to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                
+                
+            }
+            .navigationTitle("SleepEase")
+            //toolbar modifier to be added
         }
-        .padding()
+        
     }
 }
 
